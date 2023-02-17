44458ed4673ba2802b4f4ad521e7d47a9f7f61ed
Daniel Herbert <duherbert@gmail.com>
2023-02-17 15:45:52 -0800

    Task 2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int ch, a, z;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
