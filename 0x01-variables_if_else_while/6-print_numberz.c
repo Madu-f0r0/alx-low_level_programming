866d5c61e2527eb5fa96434a746ce518c2d11f5f
Daniel Herbert <duherbert@gmail.com>
2023-02-18 06:32:17 -0800

    Task 6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+
+		putchar (i);
+		putchar ('\n');
+
+	return (0);
+}
