2e5aa2880adcf5f430e60f4da73967076c3f2f1a
Daniel Herbert <duherbert@gmail.com>
2023-02-18 00:17:28 -0800

    Task 5 commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - entry point
  *
  * Return: 0 (Success)
  */
+
 int main(void)
 {
 	int i;
 
-	for(int i = 0; i <= 9; i++);
-	putchar(i);
-	putchar('\n');
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
 	return (0);
 }
