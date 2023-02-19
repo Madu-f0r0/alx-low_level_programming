899a7f87a14c231e679c742f4c192d41c0906547
Daniel Herbert <duherbert@gmail.com>
2023-02-19 14:01:48 -0800

    Task 10 commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,12 +14,18 @@ int main(void)
 	{
 		for (j = '0'; j <= '9'; j++)
 		{
-			if (i < j)
+			if (i < j && i != '8' && j != '9')
 			{
 			putchar (i);
 			putchar (j);
 			putchar (',');
 			putchar (' ');
+			}
+
+			else if (i == '8' && j == '9')
+			{
+			putchar (i);
+			putchar (j);
 			}
 		}
 	}
