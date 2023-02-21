c4ce2b9baa050153a04a13feb9d3dd589cd0ee12
Daniel Herbert <duherbert@gmail.com>
2023-02-20 19:44:11 -0800

    Task 11 commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,24 +10,26 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = '0'; i <= '7'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = '0'; j <= '8'; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
-			for (k = '0'; j <= '9'; j++)
+			for (k = '0'; k <= '9'; k++)
 			{
-				if (i < j && j < k)
+				if (i < j && i < k && j < k)
 				{
 					putchar (i);
 					putchar (j);
 					putchar (k);
-					if (i == '7' && j == '8' && k == '9')
-					{
-						continue;
-					}
-					else
+				if (i == '7' && j == '8' && k == '9')
+				{
+					break;
+				}
+				else
+				{
 					putchar (',');
 					putchar (' ');
+				}
 				}
 			}
 		}
