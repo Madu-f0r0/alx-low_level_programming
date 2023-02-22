f70ddc6a4febb268505c14310a720dc45e15df73
Daniel Herbert <duherbert@gmail.com>
2023-02-22 11:24:30 -0800

    Task 9 commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,33 +15,24 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			if (c < 10)
+			if (b == 0)
 			{
+				_putchar (c + '0');
+			}
+
+			if (c < 10 && b != 0)
+			{
+				_putchar (',');
+				_putchar (' ');
 				_putchar (' ');
 				_putchar (c + '0');
-				if (b < 9)
-				{
-					_putchar (',');
-					_putchar (' ');
-				}
-				else
-				{
-					break;
-				}
 			}
 			if (c >= 10)
 			{
+				_putchar (',');
+				_putchar (' ');
 				_putchar ((c / 10) + '0');
 				_putchar ((c % 10) + '0');
-				if (b < 9)
-				{
-					_putchar (',');
-					_putchar (' ');
-				}
-				else
-				{
-					break;
-				}
 			}
 		}
 		_putchar ('\n');
