5a967539d82c0a17b674eb4c422fde508567d504
Daniel Herbert <duherbert@gmail.com>
2023-02-23 05:30:36 -0800

    Task 2 commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: an integer to be mutiplied
+ *
+ * @b: an integer to be multiplied
+ *
+ * Return: multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
