eb5259bf80fd70fa170a6c2a127c3b7cb009bdcc
Daniel Herbert <duherbert@gmail.com>
2023-02-22 16:22:07 -0800

    Task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - sums up two integers and returns the result
+ *
+ * @a: an integer to be summed up
+ *
+ * @b: an integer to be summed up
+ *
+ * @s - represents the sum of the two integers
+ *
+ * Return: s
+ */
+
+int add(int a, int b)
+{
+	int s;
+
+	s = a + b;
+	return (s);
+}
