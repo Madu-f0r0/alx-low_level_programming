b38ec0e53bd63f940755f84a5770d166f8645832
Daniel Herbert <duherbert@gmail.com>
2023-02-23 09:19:11 -0800

    Task 6 commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,13 +10,16 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 1; i < n; i++)
+	if (n <= 0)
 	{
-		if (n <= 0)
-		{
-			break;
-		}
-		_putchar('_');
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
