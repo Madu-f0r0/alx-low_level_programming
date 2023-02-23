2342140442398318d96de93a4cf2f7f44a12c4c6
Daniel Herbert <duherbert@gmail.com>
2023-02-23 12:25:31 -0800

    Task 9 commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 1 to 100
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 5 == 0 && i != 100 && i % 3 != 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 5 != 0 && i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i == 100)
+		{
+			printf("Buzz\n");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
+
