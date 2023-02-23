a9c1454ca8da786c63b920e018b0b92a0c64bf63
Daniel Herbert <duherbert@gmail.com>
2023-02-22 19:27:47 -0800

    Task 11 commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all the natural numbers to 98
+ *
+ * @n: first number to be printed
+ */
+
+void print_to_98(int n)
+{
+	int x, y;
+
+	if (n < 98)
+	{
+		for (x = n; x <= 98; x++)
+		{
+			if (x < 98)
+			{
+				printf("%d, ", x);
+			}
+			else if (x == 98)
+			{
+				printf("%d\n", x);
+			}
+		}
+	}
+	if (n > 98)
+	{
+		for (y = n; y >= 98; y--)
+		{
+			if (y > 98)
+			{
+				printf("%d, ", y);
+			}
+			else if (y == 98)
+			{
+				printf("%d\n", x);
+			}
+		}
+	}
+}
