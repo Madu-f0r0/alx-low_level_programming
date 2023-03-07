7a402d8046e55a586ef18e18277e0f2e6c618ada
Madu-f0r0 <duherbert@gmail.com>
2023-03-07 05:42:28 -0800

    Task 1 commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: string to be concatenated into
+ *
+ * @src: string to be concatenated
+ *
+ * @n: number of bytes to be concatenated
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
