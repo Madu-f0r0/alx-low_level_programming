33457d69ba2052791e53bdcabc54c0768c6c8563
Madu-f0r0 <duherbert@gmail.com>
2023-03-07 08:42:56 -0800

    Task 4 commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ *
+ * @a: the array to be reversed
+ *
+ * @n: length of the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int tmp;
+	int i = 0;
+	n--;
+
+	while (i < n)
+	{
+		tmp = a[i];
+		a[i] = a[n];
+		a[n] = tmp;
+		++i;
+		--n;
+	}
+}
