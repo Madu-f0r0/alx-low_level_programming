efde3059aca6f7cb8c3f7714ee0a6066e670f5f3
Madu-f0r0 <duherbert@gmail.com>
2023-03-10 10:41:15 -0800

    Task 0 commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ *
+ * @argc: no. of arguments passed
+ *
+ * @argv: array of strings containing the strings passed as arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
