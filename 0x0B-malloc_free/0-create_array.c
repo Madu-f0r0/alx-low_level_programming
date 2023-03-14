bd32c0c9d21b03872fffb35f6fcb930cf971244f
Madu-f0r0 <duherbert@gmail.com>
2023-03-13 23:41:10 -0700

    Task 0 commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,10 +18,10 @@ char *create_array(unsigned int size, char c)
 
 	ptr = (char *)malloc(size * (sizeof(char)));
 
-	if (size == 0)
+	if (size == 0 || ptr == NULL)
 	{
 		return (NULL);
-	}	
+	}
 	for (i = 0; i < size; i++)
 	{
 		*(ptr + i) = c;
