50cf1677cc3a3f9a1f554317db1bfd5f3fc168d8
Maduf0r0 <duherbert@gmail.com>
2023-03-22 09:01:27 -0700

    Task 0 commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name stored in a string literal
+ * @name: variable representing the string literal
+ * @f: pointer to a function that returns a string
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
