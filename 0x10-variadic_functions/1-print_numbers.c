0e61f8c0fb836e1b5c97ffb9dd3d03e09879ebc5
Maduf0r0 <duherbert@gmail.com>
2023-03-23 04:32:13 -0700

    Task 1 commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers which are optional arguments
+ * @separator: seperates two integers to be printed
+ * @n: number of integers to be printed
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list input;
+
+	va_start(input, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+		{
+			printf("%d\n", va_arg(input, int));
+		}
+		else if (separator != NULL)
+		printf("%d%s", va_arg(input, int), separator);
+	}
+	va_end(input);
+}
