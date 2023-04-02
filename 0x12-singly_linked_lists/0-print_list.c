993075c95205375b68df17985192382b0de28bcc
Madu_f0r0 <duherbert@gmail.com>
2023-04-02 06:46:27 -0700

    Task 0 file update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,13 @@
 
 size_t print_list(const list_t *h)
 {
-	/* Declare temp pointer to traverse list, count checks no. of nodes */
 	size_t count = 0;
 	const list_t *temp;
 
-	/* Malloc not needed. *h already initialized in main function */
 	temp = h;
 
 	if (h != NULL)
 	{
-		/* Traverse list and print members; return number of nodes using count */
 		while (temp->next != NULL)
 		{
 			if (temp->str == NULL)
