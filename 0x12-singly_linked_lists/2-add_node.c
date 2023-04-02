647ffee4a596ede3963160cdf92591c380227c91
Madu_f0r0 <duherbert@gmail.com>
2023-04-02 10:04:12 -0700

    Betty correction on task 2 file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,19 +14,16 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *in_front;
 	size_t s_len = 0;
 
-	in_front = (list_t *)malloc(sizeof(list_t));
+	in_front = malloc(sizeof(list_t));
 	if (in_front == NULL)
 	{
 		return (NULL);
 	}
-	else
-	{
-		in_front->str = strdup(str);
-		while (str[s_len] != '\0')
-                	s_len++;
-		in_front->len = s_len;
-		in_front->next = *head;
-		*head = in_front;
-		return (in_front);
-	}
+	in_front->str = strdup(str);
+	while (str[s_len] != '\0')
+		s_len++;
+	in_front->len = s_len;
+	in_front->next = *head;
+	*head = in_front;
+	return (in_front);
 }
