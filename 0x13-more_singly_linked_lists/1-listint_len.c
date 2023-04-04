2f2e70857b9230fccda5334372379f4fc762aee8
Madu_f0r0 <duherbert@gmail.com>
2023-04-04 02:28:06 -0700

    initial listint_len commit

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - counts the number of elements in a linked listint_t list
+ * @h: a pointer to a listint_t
+ * Returns: the number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		node_count++;
+		h = h->next;
+	}
+	return (node_count);
+}	
