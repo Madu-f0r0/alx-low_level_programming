ea9e5e31d07077d19359c19dc6b24c616a32e154
Madu_f0r0 <duherbert@gmail.com>
2023-04-04 18:13:46 -0700

    Modification of free_listint2 function file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,14 +3,14 @@
 
 /**
  * free_listint2 - frees a listint_t list and changes value of head to NULL
- * @head - pointer to a pointer to the first node
+ * @head: pointer to a pointer to the first node
  */
 
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return;
 	temp = *head;
 	while (*head != NULL)
