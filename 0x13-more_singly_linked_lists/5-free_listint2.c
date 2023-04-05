95f1aa395cf6653d3f4c8631e85b7d1b2225c68a
Madu_f0r0 <duherbert@gmail.com>
2023-04-04 18:05:57 -0700

    Initial commit of free_listint2 function file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees a listint_t list and changes value of head to NULL
+ * @head - pointer to a pointer to the first node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head == NULL)
+		return;
+	temp = *head;
+	while (*head != NULL)
+	{
+		temp = temp->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
