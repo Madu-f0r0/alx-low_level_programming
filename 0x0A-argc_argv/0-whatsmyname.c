a6e7e98ccb6985fbedb74fd76faf0f074e8ee05c
Madu-f0r0 <duherbert@gmail.com>
2023-06-09 03:35:21 -0700

    Completed 1-args.c; updated 0-whatsmyname.c leading comments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments passed to the command line
+ * @argv: an array f the arguments passed to the command line
+ *
+ * Return: 0 if successful; 1 if error occurs
+ */
+
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to the command line
+ * @argc: the number of command line arguments
+ * @argv: an array of the command line arguments represented as strings
+ *
+ * Return: 0 if successful; 1 if error occurs
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
