5fe43566ab2bdaa98ed7357ec11fb735006af695
Madu-f0r0 <duherbert@gmail.com>
2023-06-15 11:51:33 -0700

    Completed 3-array_range.c program

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+int *array_range(int min, int max)
+{
+	int i, j;
+	int *arr;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	arr = malloc(((max - min) + 1) * sizeof(int));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0, j = min; j <= (max - min); i++, j++)
+	{
+		arr[i] = j;
+	}
+	return (arr);
+}
