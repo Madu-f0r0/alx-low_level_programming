d0e20c9a7fd22a478568ef805780d608f700cedf
Madu-f0r0 <duherbert@gmail.com>
2023-06-19 08:15:54 -0700

    Updated dog.h header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * struc dog - defines the properties of individual dogs
+ * struct dog - defines the properties of individual dogs
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the name of the owner of the dog
@@ -14,5 +14,11 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
