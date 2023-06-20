ba92ca4c08c216fcd0265d686b284fb3c7129341
Madu-f0r0 <duherbert@gmail.com>
2023-06-19 23:34:03 -0700

    Modified 1-init_dog response to NULL parameters

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,7 +12,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (name == NULL || age == 0 || owner == NULL)
+	if (d == NULL || name == NULL || age == 0 || owner == NULL)
 	{
 		return;
 	}
