3427ac567eff2e676e1445793af9736d19f3bb8b
Madu-f0r0 <duherbert@gmail.com>
2023-06-22 07:16:32 -0700

    Modified print format for 1-print_numbers.c program

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -34,7 +34,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(numbers, int));
 		for (i = 1; i < n; i++)
 		{
-			printf("%s %d", separator, va_arg(numbers, int));
+			printf("%s%d", separator, va_arg(numbers, int));
 		}
 		printf("\n");
 	}
