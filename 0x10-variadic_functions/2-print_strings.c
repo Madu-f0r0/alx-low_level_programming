fa9f835711a4d4830ec14ecb2bfd49c6ab6167e9
Madu-f0r0 <duherbert@gmail.com>
2023-06-23 00:26:10 -0700

    Completed 2-print_strings.c program

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,13 +11,15 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+	char *buffer;
+
 	va_list strings;
 
 	if (n == 0)
 		return;
 
 	va_start(strings, n);
-	
+
 	for (i = 0; i < n; i++)
 	{
 		buffer = va_arg(strings, char *);
