545b42cc4ffced8f487e12b79162eada6680fb94
Madu-f0r0 <duherbert@gmail.com>
2023-06-23 14:43:02 -0700

    Modified string separation in 3-print_all.c program

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,30 +4,64 @@
 #include <string.h>
 
 /**
- * print_all - prints all the optional arguments passed
+ * arg_counter - counts the no. of valid format specifiers in a format string
  * @format: the format string
+ *
+ * Return: the number of arguments
  */
 
-void print_all(const char * const format, ...)
+unsigned int arg_counter(const char * const format)
 {
-	unsigned int i = 0;
-	char *buffer = NULL;
-	va_list list;
-
-	va_start(list, format);
+	unsigned int counter = 0, i = 0;
 
 	while (format != NULL && i < strlen(format))
 	{
 		switch (format[i])
 		{
 			case 'c':
+				counter++;
+				break;
+			case 'i':
+				counter++;
+				break;
+			case 'f':
+				counter++;
+				break;
+			case 's':
+				counter++;
+		}
+		i++;
+	}
+	return (counter);
+}
+
+/**
+ * print_all - prints all the optional arguments passed
+ * @format: the format string
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i = 0, j = 0, validFormatSwitch;
+	char *buffer = NULL;
+	va_list list;
+
+	va_start(list, format);
+	while (format != NULL && i < strlen(format))
+	{
+		validFormatSwitch = 0;
+		switch (format[i])
+		{
+			case 'c':
 				printf("%c", va_arg(list, int));
+				validFormatSwitch = 1;
 				break;
 			case 'i':
 				printf("%i", va_arg(list, int));
+				validFormatSwitch = 1;
 				break;
 			case 'f':
 				printf("%f", va_arg(list, double));
+				validFormatSwitch = 1;
 				break;
 			case 's':
 				buffer = va_arg(list, char *);
@@ -37,6 +71,12 @@ void print_all(const char * const format, ...)
 					break;
 				}
 				printf("%s", buffer);
+				validFormatSwitch = 1;
+		}
+		if (validFormatSwitch == 1 && j < (arg_counter(format)) - 1)
+		{
+			printf(", ");
+			j++;
 		}
 		i++;
 	}
