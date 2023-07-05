b8f35517fea339d251488bcf08c8a41cba662051
Madu-f0r0 <duherbert@gmail.com>
2023-07-05 07:09:49 -0700

    Completed 4-free_listint.c program

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - frees every element of a listint_t list
+ * @head: points to the first element of the linked list
+ *
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *freeNode;
+
+	if (head)
+	{
+		freeNode = head;
+		while (head)
+		{
+			freeNode = freeNode->next;
+			free(head);
+			head = freeNode;
+		}
+	}
+}
