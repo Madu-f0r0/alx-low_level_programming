fc14f5e70123903e0a3468182afa12bebe7c0129
Madu-f0r0 <duherbert@gmail.com>
2023-07-07 18:34:17 -0700

    Completed task 2 function

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * get_bit - selects the value of a bit at a given index
+ * @n: the number whose bit value is to be selected
+ * @index: the index to select the bit value
+ *
+ * Return: the bit value selected
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	char arr[64];
+	int i = 0;
+
+	if (n < 2)
+	{
+		arr[i++] = n;
+		arr[i] = '\0';
+	}
+	else
+	{
+		while (n)
+		{
+			arr[i] = (n & 1);
+			n = n >> 1;
+			i++;
+		}
+		arr[i] = '\0';
+	}
+
+	if ((int)index > (i - 1))
+	{
+		return (-1);
+	}
+
+	i = 0;
+	while (i <= (int)index)
+	{
+		i++;
+	}
+	i--;
+	return (arr[i]);
+}
