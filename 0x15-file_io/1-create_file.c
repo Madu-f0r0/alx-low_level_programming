fe5116ba6e5ed6ebfb83ffb63fca58df27bb5a3e
Madu-f0r0 <duherbert@gmail.com>
2023-07-10 11:06:05 -0700

    Completed task 1 function

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * create_file - creates a file and writes a string into it
+ * @filename: the name of the file to be created
+ * @text_content: the string to be written  into the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fileDes, len = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fileDes = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fileDes == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+	}
+
+	if (write(fileDes, text_content, len) == -1)
+	{
+		return (-1);
+	}
+
+	return (1);
+}
