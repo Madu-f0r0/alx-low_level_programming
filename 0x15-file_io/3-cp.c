3b9c5d8e1310b0264f3422b38b802ef9936276d2
Madu-f0r0 <duherbert@gmail.com>
2023-07-11 16:24:07 -0700

    Updated task 3 function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,54 +1,55 @@
 #include "main.h"
 
 /**
- * copy_text - copies the content of a file to another file
- * @file_from: .
- * @file_to: .
+ * copy_func - copies the content of a file to another file
+ * @file_orig: the file whose content is to be copied into another file
+ * @file_cpy: the file to be copied into
  */
-void copy_text(char *file_from, char *file_to)
+void copy_func(char *file_orig, char *file_cpy)
 {
-	int fdf, fdt, i = 0;
+	int fdOrig, fdCpy, index;
+	ssize_t rRet, wRet, clOrig, clCpy;
 	char buff[1024];
-	ssize_t rRet, wRet, fCl, tCl;
 
-	fdf = open(file_from, O_RDONLY);
-	fdt = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fdOrig = open(file_orig, O_RDONLY);
+	fdCpy = open(file_cpy, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
+	index = 0;
 	do {
-		rRet = read(fdf, buff, 1024);
-		if (fdf == -1 || rRet == -1)
+		rRet = read(fdOrig, buff, 1024);
+		if (fdOrig == -1 || rRet == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_orig);
 			exit(98);
 		}
 
-		wRet = write(fdt, buff, rRet);
-		if (fdt == -1 || wRet == -1)
+		wRet = write(fdCpy, buff, rRet);
+		if (fdCpy == -1 || wRet == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_cpy);
 			exit(99);
 		}
-		i++;
-	} while (rRet != 0);
+		index++;
+	} while (rRet == 1024);
 
-	fCl = close(fdf);
-	if (fCl == -1)
+	clOrig = close(fdOrig);
+	if (clOrig == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdf);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdOrig);
 		exit(100);
 	}
-	tCl = close(fdt);
-	if (tCl == -1)
+	clCpy = close(fdCpy);
+	if (clCpy == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdt);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdCpy);
 		exit(100);
 	}
 }
 
 /**
  * main - receives two CL arguments (file_from) and (file_to)
- * @argc: .
- * @argv: .
+ * @argc: the number of CL arguments
+ * @argv: an array of the CL arguments
  *
  * Return: 0 on success
  */
@@ -60,6 +61,6 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	copy_text(argv[1], argv[2]);
+	copy_func(argv[1], argv[2]);
 	return (0);
 }
