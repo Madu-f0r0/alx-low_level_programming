5bf0bc5106faf36a17bf052b0a4bbb1c6425eb94
Madu-f0r0 <duherbert@gmail.com>
2023-08-19 17:13:36 +0100

    Refactored print_times_table function

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,76 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - prints a number in the print_times_table below
+ * @num: the number to be printed
+ * @first: indicates if the number is the first in the row
+ */
+void print_number(int num, int first)
+{
+	int i;
+
+	if (num < 10)
+	{
+		if (first == 0)
+		{
+			for (i = 0; i < 3; i++)
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('0' + num);
+	}
+	else if (num >= 10 && num < 100)
+	{
+		if (first == 0)
+		{
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar('0' + (num / 10));
+		_putchar('0' + (num % 10));
+	}
+	else
+	{
+		if (first == 0)
+		{
+			_putchar(' ');
+		}
+		_putchar('0' + (num / 100));
+		_putchar('0' + ((num / 10) % 10));
+		_putchar('0' + (num % 10));
+	}
+}
 
 /**
  * print_times_table - prints time tables of n
- *
  * @n: is the number whose time table is to be printed
  */
 void print_times_table(int n)
-{	
-	int x, y, z;
+{
+	int x, y, first;
 
 	if (n >= 0 && n <= 15)
-	{	
+	{
 		for (x = 0; x <= n; x++)
-		{	
+		{
 			for (y = 0; y <= n; y++)
-			{	
-				z = x * y;
-
-				if (y == 0)
-				{	
-					_putchar(z + '0');
+			{
+				if (y)
+				{
+					first = 0;
+					_putchar(',');
 				}
-				else if (y > 0)
-				{	
-					if (z < 10)
-					{	
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(z + '0');
-					}
-					else if (z >= 10 && z < 100)
-					{	
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar((z / 10) + '0');
-						_putchar((z % 10) + '0');
-					}
-					else if (z >= 100)
-					{	
-						_putchar(',');
-						_putchar(' ');
-						_putchar((z / 100) + '0');
-						_putchar(((z / 10) % 10) + '0');
-						_putchar((z % 10) + '0');
-					}}}
+				else
+				{
+					first = 1;
+				}
+
+				print_number((x * y), first);
+			}
 			_putchar('\n');
-		}}}
+		}
+
+	}
+}
