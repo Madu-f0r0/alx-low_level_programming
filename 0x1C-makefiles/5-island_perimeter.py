c4f930f2c9edee30bd64b8de3d356e8ff989a8a1
Madu-f0r0 <duherbert@gmail.com>
2023-10-25 20:25:39 +0100

    Refactored island_perimeter code

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,6 +14,9 @@ def island_perimeter(grid):
     for i in range(1, len(grid) - 1):
         for j in range(1, len(grid[i]) - 1):
             if grid[i][j] == 1:
+                if grid[i - 1][j] == 0 and grid[i][j + 1] == 0:
+                    if grid[i + 1][j] == 0 and grid[i][j - 1] == 0:
+                        continue
                 if grid[i - 1][j] == 0:
                     p += 1
                 if grid[i][j + 1] == 0:
